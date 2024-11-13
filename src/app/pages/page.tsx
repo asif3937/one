5ae56f6afdc9a3c876014b828d4a44b1239dcc58
asif3937 <160737982+asif3937@users.noreply.github.com>
2024-11-13 13:18:22 +0500

    Add files via upload

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Main(){
+    return(
+        <div>
+            <h1>Muhammad Asif Khan</h1>
+            <link href='/my'>My page</link>
+            <link href='/qualification'>qualification page</link>
+            <link href='/career'>Career page</link>
+            
+        </div>
+    );
+}
